dd1b4b1873dc63d9318cfe777e59fd906c4d351c
tejaswinigaddam26 <tejaswininetha5@gmail.com>
2025-07-09 22:41:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Stock from './Stock';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Stock />
+    </div>
+  );
+}
+
+export default App;
